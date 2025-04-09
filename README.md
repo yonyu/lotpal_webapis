e7344303e7332b67b9c719bc6a695b96f108688b
Yong Yu <ypyu@hotmail.com>
2025-04-08 17:43:13 -0700

    Add a health check endpoint

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Installation
+# LotPal Application Backend
 
 ```
 cargo new lotpal_webapis
@@ -6,6 +6,8 @@ cargo new lotpal_webapis
 git branch -M master
 git remote add origin https://github.com/yonyu/lotpal_webapis.git
 git push -u origin master
+
+cargo add actix-web@4
 ```
 
-
+http://127.0.0.1:8080/healthz
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer};
+
+#[get("/healthz")]
+async fn liveness() -> &'static str {
+    "ok\r\n"
+}
+
+#[actix_web::main]
+ async fn main() -> std::io::Result<()> {
+    let app = || App::new().service(liveness);
+
+    HttpServer::new(app)
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
